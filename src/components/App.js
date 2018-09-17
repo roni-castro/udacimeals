8b53e8baeefc031bd31c5403c29b3e051984efe2
Roni <roni.c.castro@gmail.com>
2018-09-16 23:51:11 -0300

    Use provider to pass props to components

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,27 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import '../App.css';
-import { addRecipe } from '../actions';
+import { connect } from 'react-redux';
 
 class App extends Component {
 
-  state = {
-    calendar: null
-  }
-
-  componentDidMount() {
-    const { store } = this.props
-    store.subscribe(() => {
-      this.setState(() => {
-        calendar: store.getState()
-      })
-    })
-  }
-
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday', 
-      recipe: {
-        label: this.input.value
-      },
-      meal: 'breakfast'
-    }))
-    this.input.value = ''
-  }
-
   render() {
+    console.log(this.props)
     return (
       <div>
-        <input
-          type='text'
-          ref={(input) => this.input = input}
-          placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-
-        <pre>
-          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
+       HELLO WORLD
       </div>
     )
   }
 }
 
-export default App;
+function mapStateToProps(calendar) {
+  return {
+    calendar: Object.keys(calendar)
+    .map((day) => ({
+      day,
+      meals: {...calendar[day]}
+    }))
+  }
+}
+
+export default connect(mapStateToProps)(App);
